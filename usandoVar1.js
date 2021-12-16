1d174d93cdb2ed75421bfd00ee88c86191d288ac
Raphael Alves <36492237+hollowgreen@users.noreply.github.com>
2021-12-16 14:53:46 -0300

    Add files via upload

---
+++ usandoVar1.js
@@ -0,0 +1,19 @@
+{
+    {
+        {
+            {
+                var sera = 'Será???'
+                console.log(sera)
+            }
+
+        }
+    }
+}
+console.log(sera)
+
+function teste(){
+    var local = 123
+    console.log(local)
+}
+teste()
+//console.log(local) não defenido ja que esta defenido na chave {}
